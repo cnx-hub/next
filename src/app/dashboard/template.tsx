23e192609d51b40bd593ccd7096961551ab0b78a
judy.chen <judy.chen@temu.com>
2025-04-16 13:01:29 +0800

    1

--- src/app/dashboard/template.tsx
+++ src/app/dashboard/template.tsx
@@ -3,6 +3,7 @@ import React,{useState} from 'react'
 
 
 // 切换路由时 template 会重新渲染， layout 不会
+// layout 不会渲染仅限于一个大的路由下，完完全全切换路由也重新渲染
 
 export default function Template({children}: {children: React.ReactNode}) {
     const [count, setCount] = useState(0);
