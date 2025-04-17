82bd8c6be3bb9a7fa7a58ac8112022ec98dcf40b
judy.chen <judy.chen@temu.com>
2025-04-17 09:04:18 +0800

    1

---
+++ src/app/test/not-found.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function notFound() {
+  return (
+    <div>我是局部的 not - found 文件</div>
+  )
+}
---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { notFound } from 'next/navigation'
+
+export default function page() {
+    notFound();
+  return (
+    <div></div>
+  )
+}
