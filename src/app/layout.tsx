4105dc1d89c96daaef087c58761856c84ee33620
judy.chen <judy.chen@temu.com>
2025-04-27 09:33:21 +0800

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,13 +13,29 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  team,
+  analytics
 }: Readonly<{
   children: React.ReactNode;
+  team: React.ReactNode;
+  analytics: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AntdRegistry>{children}</AntdRegistry>
+        <AntdRegistry>
+          <div className="container mx-auto">
+            <div className="flex justify-center text-blue-500 p-6 gap-6">
+              <Link href="/">Home</Link>
+              <Link href="/visitors">Visitors</Link>
+            </div>
+            <div className="flex gap-6">
+              {team}
+              {analytics}
+            </div>
+            {children}
+          </div>
+        </AntdRegistry>
       </body>
     </html>
   );
--- src/app/test/not-found.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function notFound() {
-  return (
-    <div>我是局部的 not - found 文件</div>
-  )
-}
